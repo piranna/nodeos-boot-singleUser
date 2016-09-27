9c80e57e8bc57e80df6bd8f43740c8de0da5cd35
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2016-09-27 08:07:44 +0200

    Split from `nodeos-mount-filesystems`

---
+++ README.md
@@ -0,0 +1,5 @@
+# NodeOS boot singleUser
+
+Load and prepare a minimal system to exec a diskless single process / single
+user session on NodeOS. The per-user root filesystem will need to be located on
+`/tmp` on the initram tarfile.
--- server.js
+++ server.js
@@ -1,92 +1,40 @@
 #!/usr/bin/env node
 
-const readFile = require('fs').readFile
+const repl = require('repl')
 
-const startRepl = require('nodeos-mount-utils').startRepl
+const run = require('jocker').run
 
-const boot = require('.')
+const basicEnvironment = require('.')
 
 
 const MOUNTPOINT = '/tmp'
 
 
 /**
- * This functions takes the `cmdline` from `/proc/cmdline` **showed below in
- * the example** and splits it into key/value pairs
- * @access private
- * @param  {String} cmdline This string contains information about the
- *                          initrd and the root partition
- * @return {Object}         It returns a object containing key/value pairs
- *                          if there is no value for the key then its just true.
- *                          **For more Information, look at the example**
- * @example
- *   var cmdline1 = 'initrd=\\initramfs-linux.img root=PARTUUID=someuuidhere\n'
- *   var cmdline2 = 'somevar root=PARTUUID=someuuidhere\n'
- *
- * 	 var res1 = linuxCmdline(cmdline1)
- * 	 var res2 = linuxCmdline(cmdline2)
- * 	 console.log(res1)
- * 	 //-> { initrd: '\\initramfs-linux.img',root: 'PARTUUID=someuuidhere' }
- * 	 console.log(res2)
- * 	 //-> { somevar: true, root: 'PARTUUID=someuuidhere' }
- */
-function linuxCmdline(cmdline)
-{
-  var result = {}
-
-  cmdline.trim().split(' ').forEach(function(arg)
-  {
-    arg = arg.split('=')
-
-    var key = arg.shift()
-    var val = true
-
-    if(arg.length)
-    {
-      val = arg.join('=').split(',')
-      if(val.length === 1) val = val[0]
-    }
-
-    result[key] = val
-  })
-
-  return result
-}
-
-/**
  * This error handler traces the error and starts a Node.js REPL
  *
- * @param  {Error} error The error that gets traced
+ * @param {Error} error The error that gets traced
  */
 function onerror(error)
 {
   console.trace(error)
-  startRepl('NodeOS-mount-filesystems')
+
+  repl.start('NodeOS-boot-singleUser> ').on('exit', function()
+  {
+    console.log('Got "exit" event from repl')
+    process.exit(2)
+  })
 }
 
 
-boot.basicEnvironment(function(error)
+basicEnvironment(function(error)
 {
   if(error) return onerror(error)
 
-  // Get Linux kernel command line arguments
-  readFile('/proc/cmdline', 'utf8', function(error, data)
+  run(MOUNTPOINT, '/init', {PATH: '/bin'}, function(error)
   {
     if(error) return onerror(error)
 
-    var cmdline = linuxCmdline(data)
-
-    // Mount users filesystem
-    boot.mountUsersFS(MOUNTPOINT, cmdline, function(error)
-    {
-      if(error) return onerror(error)
-
-      boot.prepareSessions(MOUNTPOINT, cmdline.single, function(error)
-      {
-        if(error) return onerror(error)
-
-        // KTHXBYE >^.^<
-      })
-    })
+    // KTHXBYE >^.^<
   })
 })
